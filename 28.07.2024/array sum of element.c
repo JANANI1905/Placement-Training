dae6766a3bd19dcab2278fbef2b47b693a82e618
JANANI1905 <151322389+JANANI1905@users.noreply.github.com>
2024-07-28 18:59:07 +0530

    Update array sum of element.c

--- 28.07.2024/array sum of element.c
+++ 28.07.2024/array sum of element.c
@@ -1,8 +1,10 @@
     #include<stdio.h>
-    int main(){
+    int main()
+    {
         int arr[]={1,2,3,4,5};
         int sum=0;
-        for(int i=0;i<5;i++){
+        for(int i=0;i<5;i++)
+        {
             sum+=arr[i];
         }
 
